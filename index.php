0f6d2c50778aa26fb7eb14f0bf9e257c335cbdd2
Rogério Pereira <rogerio@rogeriopereira.info>
2015-11-11 11:18:18 -0200

    Importaçao Github

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         {
             if (file_exists("{$pasta}/{$classe}.php"))
             {
-                include_once "{$pasta}/{$classe}.php";
+                include_once "{$pasta}/{$classe}.php"; 
             }
         }
     }
